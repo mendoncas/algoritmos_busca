0587e126f0d34b2ed08161ab30e8a5b6fd1512f5
730549390 <ricardocesarfilho@unifor.br>
2023-04-10 17:01:21 -0300

    atributo custo para nós

--- ma.py
+++ ma.py
@@ -2,35 +2,21 @@ import queue
 from collections import deque
 from math import dist
 
-"""
-A*
-BCU
-DFS
-
-"""
-
-       
-
 class Node:
-    
     id = -1
     pai = None
-    coordenadas = [0, 0]
+    custo = 0
     
     def __init__(self,id):
         self.id = id
-        self.coordenadas = [self.id, self.id]
-
     
 
 class Grafo:
     
-    matriz = []
-    matriz_arestas = []
+    matriz = [] #matriz de custo dos caminhos, alterada pelos algoritmos
     n = 0
     direcionado = False
     heuristica = []
-
    
     def __init__(self,n,direcionado): 
         self.n = n
@@ -38,20 +24,18 @@ class Grafo:
         self.heuristica = []
         for i in range(n):
             self.matriz.append([0]*n)            
-            self.matriz_arestas.append([0]*n)            
     
 
-
-    def custo(self, no: Node):
+    def custo_aresta(self, no: Node):
         return self.matriz[no.id][no.pai.id]
 
+    def custo_no(self, no: Node):
+        return no.custo
 
     def addAresta(self, s, t, peso=1):
         if(not self.direcionado):
             self.matriz[t][s]=peso
-            self.matriz_arestas[t][s]=peso
         self.matriz[s][t]=peso
-        self.matriz_arestas[s][t]=peso
         
 
     def printMatriz(self):
@@ -65,10 +49,9 @@ class Grafo:
         print()
     
 
-    def bcu(self, start: int, target: int, log=True):
+    def custo_uniforme(self, start: int, target: int, log=True):
         l = []
-        custo = 0
-        expandidos = 0
+        expandidos = []
         
         node = Node(start)
         node.pai = Node(-1)       
@@ -77,27 +60,24 @@ class Grafo:
         
         while(not (len(l) == 0)):
             aux = l.pop(0)
-            expandidos += 1
-            if(aux.pai.id > -1):
-                custo = self.matriz[aux.id][aux.pai.id]
-            print("expandindo {x}, custo atual: {c}".format(x=aux.id, c=custo))
+            expandidos.append(aux.id)
+            print("expandindo ", aux.id)
             
             if(aux.id == target):
-                print("expandidos: ", expandidos)
+                print("expandidos: ", len(expandidos))
                 return aux
 
             for i in range(self.n):                
                 if(self.matriz[aux.id][i] >= 1 and i != aux.pai.id):
                     node = Node(i)
                     node.pai = aux
-                    self.matriz[node.id][node.pai.id] += self.custo(aux)
+                    node.custo = self.custo_aresta(node) + aux.custo
                     l.append(node)
-                    l.sort(key = self.custo)
+                    l.sort(key = self.custo_no)
 
-            # print("estado da fila: {n}".format(n=self.mostraNos(l)))
             output = []
             for i in l:
-                output.append({"nó": i.id, "custo": self.custo(i)})
+                output.append({"nó": i.id, "custo": self.custo_aresta(i)})
             if log:
                 print("estado atual da fila: ", output)
 
@@ -107,8 +87,6 @@ class Grafo:
 
     def a_estrela(self, start: int, target: int, log =True):
         l = []
-        # nos_buscados = []
-        custo = 0
         expandidos = 0
         
         node = Node(start)
@@ -119,15 +97,10 @@ class Grafo:
         while(not (len(l) == 0)):
             aux = l.pop(0)
             expandidos += 1
-            if(aux.pai.id > -1):
-                # self.matriz[aux.id][aux.pai.id] += self.custo(aux) + self.heuristica[aux.id]
-                custo = self.custo(aux)
 
-            print("expandindo {x}, custo atual: {c}".format(x=aux.id, c=custo))
+            print("expandindo ", aux.id)
             
             if(aux.id == target):
-                # if(custo < menor_custo):
-                #     menor_custo = custo
                 print("expandidos: ", expandidos)
                 return aux
 
@@ -135,27 +108,22 @@ class Grafo:
                 if(self.matriz[aux.id][i] >= 1 and i != aux.pai.id ):
                     node = Node(i)
                     node.pai = aux
-                    # self.matriz[node.id][node.pai.id] += self.custo(aux) + int(dist(node.coordenadas, [target, target]))
-                    self.matriz[node.id][node.pai.id] += self.custo(aux) + self.heuristica[aux.id]
+                    node.custo = self.heuristica[node.id] + self.custo_aresta(node) + aux.custo
                     l.append(node)
+                    l.sort(key = self.custo_no)
 
-                    l.sort(key = self.custo)
-
-            # print("estado da fila: {n}".format(n=self.mostraNos(l)))
             output = []
             for i in l:
-                output.append({"nó": i.id, "custo": self.custo(i)})
+                output.append({"nó": i.id, "custo": self.custo_aresta(i)})
             if log:
                 print("estado atual da fila: ", output)
 
             print()
-        print("o menor custo foi ", menor_custo)
         return aux
 
- 
+
     def melhor_escolha(self, start: int, target: int, log=True):
         l = []
-        custo = 0
         expandidos = 0
         
         node = Node(start)
@@ -166,9 +134,7 @@ class Grafo:
         while(not (len(l) == 0)):
             aux = l.pop(0)
             expandidos += 1
-            if(aux.pai.id > -1):
-                custo = self.matriz[aux.id][aux.pai.id]
-            print("expandindo {x}, custo atual: {c}".format(x=aux.id, c=custo))
+            print("expandindo ", node.id)
             
             if(aux.id == target):
                 print("expandidos: ", expandidos)
@@ -178,22 +144,20 @@ class Grafo:
                 if(self.matriz[aux.id][i] >= 1 and i != aux.pai.id):
                     node = Node(i)
                     node.pai = aux
-                    # self.matriz[node.id][node.pai.id] += self.custo(aux)
                     l.append(node)
-                    l.sort(key = self.custo)
+                    l.sort(key = self.custo_aresta)
 
-            # print("estado da fila: {n}".format(n=self.mostraNos(l)))
             output = []
             for i in l:
-                output.append({"nó": i.id, "custo": self.custo(i)})
+                output.append({"nó": i.id, "custo": self.custo_aresta(i)})
             if log:
                 print("estado atual da fila: ", output)
 
             print()
         return aux
- 
+    
 
-    def bp(self, start: int, target: int):
+    def busca_profundidade(self, start: int, target: int):
         stack = deque()
         
         node = Node(start)
@@ -220,7 +184,7 @@ class Grafo:
         return aux
  
     
-    def bl(self,s,t):
+    def busca_largura(self,s,t):
         q = queue.Queue()
         expandidos = 0
         
@@ -251,14 +215,13 @@ def caminho(no: Node):
     print("caminho encontrado: ")
     while(no.id != -1):
         print(no.id)
-        custo_caminho += g.matriz_arestas[no.id][no.pai.id]
+        custo_caminho += g.custo_aresta(no)
         no = no.pai
     print("custo do caminho encontrado: ", custo_caminho)
     print()
 
 
-# g = Grafo(10,False)
-g = Grafo(30,False)
+g = Grafo(10,False)
 g.heuristica =[
     10,
     9,
@@ -272,34 +235,32 @@ g.heuristica =[
     0
 ]
 
-
 g.addAresta(0, 2, 3)
 g.addAresta(1, 3, 7)
 g.addAresta(2, 3, 3)
 g.addAresta(3, 5, 2)
 g.addAresta(5, 4, 3)
 g.addAresta(3, 6, 3)
-g.addAresta(6, 9, 2)
+g.addAresta(6, 9, 5)
 g.addAresta(4, 7, 7)
 g.addAresta(4, 8, 5)
-g.addAresta(8, 9, 1)
+g.addAresta(8, 9, 55)
 
 g.addAresta(3, 7, 1)
 g.addAresta(7, 4, 1)
 g.addAresta(4, 2, 1)
 
-# print("BUSCA EM LARGURA")
-# caminho(g.bl(0, 9))
+print("BUSCA EM LARGURA")
+caminho(g.busca_largura(0, 9))
 
-# print("BUSCA EM PROFUNDIDADE")
-# caminho(g.bp(0, 9))
+print("BUSCA EM PROFUNDIDADE")
+caminho(g.busca_profundidade(0, 9))
 
 # print("BUSCA DE CUSTO UNIFORME")
-# caminho(g.bcu(0, 9, False))
+# caminho(g.custo_uniforme(0, 9, True))
 
 # print("BUSCA A*")
 # caminho(g.a_estrela(0, 9, True))
 
-# print("BUSCA MELHOR ESCOLHA")
-# caminho(g.melhor_escolha(0, 9, False))
-
+print("BUSCA MELHOR ESCOLHA")
+caminho(g.melhor_escolha(0, 9, False))
